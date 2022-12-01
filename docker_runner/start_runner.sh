508a2417935b2b19a6f85a51dd7676129c55b7ec
Nacho <nacho@sixsq.com>
2022-12-01 16:30:01 +0100

    Fix gracious exit of docker action runner

--- docker_runner/start_runner.sh
+++ docker_runner/start_runner.sh
@@ -10,14 +10,14 @@ cd /home/runner/actions-runner
 
 echo ${ORGANIZATION}
 
-./config.sh --unattended --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --replace --name validation_runner_1 --labels validation-runner
+./config.sh --unattended --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --replace --name ${HOSTNAME} --labels validation-runner
 
 cleanup() {
     echo "Removing runner..."
     ./config.sh remove --token ${REG_TOKEN}
 }
 
+trap 'cleanup; exit 143' SIGTERM
 trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
 
 ./run.sh & wait $!
---
+++ docker_runner/start_runner_fleet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create a shared volume among containers
+docker volume create --driver local --opt type=tmpfs --opt device=/Users/runner/.ssh  testbed_keys
+
+# Populate that volume with the ssh keys
+docker container create --name copy_to_vol -v testbed_keys:/opt/ hello-world
+docker cp ~/.ssh/ copy_to_vol:/opt/test
+docker rm copy_to_vol
+
+n=2
+for (( i = 0; i < n; i++ )); do
+    docker run --detach --rm --name validation_runner_${i} --hostname validation_runner_$ --env RUNNER_TOKEN=$RUNNER_TOKEN --env ORGANIZATION=nuvlaedge  -v testbed_keys:/home/runner/.ssh/ validation-runner:0.0.1a1
+done
